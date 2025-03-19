a57ecfc75285a41f926d24f0dfbab8f49b1f0988
serendipitywgy <serendipitywgy@gmail.com>
2025-03-20 00:25:01 +0800

    增加.gitignore文件

--- Learn_OpenCV_in_4_hours/chapter01/chapter1.cpp
+++ Learn_OpenCV_in_4_hours/chapter01/chapter1.cpp
@@ -1,6 +1,17 @@
 #include <print>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+
+
 int main(int argc, char** argv){
 
-    std::println("hello, from chapter01!");
+    std::string path = "../Resources/book.jpg";
+    cv::Mat img = cv::imread(path, cv::IMREAD_COLOR);
+    cv::imshow("Image", img);
+
+    cv::waitKey(0);
+    // std::println("hello, from chapter01!");
     return 0;
 }
